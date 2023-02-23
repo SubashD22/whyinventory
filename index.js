7529407ba4dd11afccb4c1fffb295749cd08cc69
SubashD22 <101434737+SubashD22@users.noreply.github.com>
2023-02-23 16:07:12 +0530

    connected to mongo

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config();
+const Port = process.env.PORT || 5000;
+const app = express();
+mongoose.set('strictQuery',false)
+mongoose.connect(`mongodb+srv://subashD:sub0222@mern.vjalb.mongodb.net/whyinventory`,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('connected to db')).catch(err=> console.log(err))
+
+app.listen(Port,()=>{
+    console.log(`connected to Port ${Port}`)
+})
